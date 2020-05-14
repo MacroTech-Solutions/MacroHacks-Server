e6b25ef9158d4174aba70fa372982dfd9cdb9f1c
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2020-05-14 18:41:07 -0400

    Remove bor code

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-var Discord = require('discord.io');
-var logger = require('winston');
 var admin = require('firebase-admin');
 const path = require('path');
 var bodyParser = require('body-parser');
@@ -13,47 +11,6 @@ var app = admin.initializeApp({
 });
 
 var database = admin.database();
-
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-  token: "NzEwMTc2NDQ2MjIzNDgzMDIx.XrwpZA.DhUEXWGgQxR1hmrJ2dryAxcaHao",
-  autorun: true
-});
-bot.on('ready', function (evt) {
-  logger.info('Connected');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
-    args = args.splice(1);
-    if (channelID == "710182346640326807") {
-      switch (cmd) {
-        case 'verify':
-          bot.sendMessage({
-            to: channelID,
-            message: 'Verifying...'
-          });
-          break;
-      }
-    }
-    switch (cmd) {
-      case 'status':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Active'
-        });
-        break;
-    }
-  }
-});
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
